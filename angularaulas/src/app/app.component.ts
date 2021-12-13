098fb8e77e6a9f8c82cb8a2cf5be22507b1a5b45
Henrique Franco <henrique.franco11@yahoo.com.br>
2021-12-12 22:55:11 -0300

    conteúdo da aula de templates e variáveis

--- angularaulas/src/app/app.component.ts
+++ angularaulas/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angularaulas';
+  nome = "Henrique";
+  idade = 26
+
+  nomes = ['Henrique', 'Vanessa', 'Rosemeri', 'Karina'];
+  nomePrincipal = this.nomes[0];
 }
