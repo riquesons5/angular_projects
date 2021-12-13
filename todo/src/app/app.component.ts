5e3a9d2ed950e6199285bf9ecc3e96a58f0777ec
Henrique Quevedo <henrique.quevedo@useall365.onmicrosoft.com>
2021-12-13 15:00:37 -0300

    exemplos até aula de exibição de dados

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
+  // template: '<p>Meu template</p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = []; // resultado: []; // o ':' é usado para tipar a variável. 'any' significa 'qualquer tipo'
+  // public todos: any[]; // resultado: undefined
+  public title: String = 'Minhas Tarefas';
+
+  constructor() { // é chamado toda vez que o componente é iniciado
+    this.todos.push('passear com o cachorro');
+    this.todos.push('ir ao supermercado');
+    this.todos.push('cortar o cabelo');
+  }
+
+  alterarTexto() {
+    this.title = 'Teste';
+  }
+
 }
