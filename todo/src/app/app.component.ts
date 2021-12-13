e4e64a81f332bec736854f0b63f11316b2d3a9a4
Henrique Quevedo <henrique.quevedo@useall365.onmicrosoft.com>
2021-12-13 15:38:55 -0300

    conteúdo até aula de ngIf

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,24 +1,19 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root', // <app-root>
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: '<p>Meu template</p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = []; // resultado: []; // o ':' é usado para tipar a variável. 'any' significa 'qualquer tipo'
-  // public todos: any[]; // resultado: undefined
+  public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
 
-  constructor() { // é chamado toda vez que o componente é iniciado
-    this.todos.push('passear com o cachorro');
-    this.todos.push('ir ao supermercado');
-    this.todos.push('cortar o cabelo');
-  }
-
-  alterarTexto() {
-    this.title = 'Teste';
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
   }
 
 }
